4d96a7373b1dc26a94988da3756e2ef6a96e96ef
kalisowski <lisekgda3@gmail.com>
2023-12-18 21:41:19 +0100

    home page init

---
+++ src/app/pages/home-page/home-page.component.ts
@@ -0,0 +1,29 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { AuthService } from '@auth0/auth0-angular';
+import { ButtonModule } from 'primeng/button';
+import { RippleModule } from 'primeng/ripple';
+import { Observable } from 'rxjs';
+import { ProgressSpinnerModule } from 'primeng/progressspinner';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-home-page',
+  standalone: true,
+  imports: [CommonModule, ButtonModule, RippleModule, ProgressSpinnerModule],
+  templateUrl: './home-page.component.html',
+  styleUrl: './home-page.component.scss',
+})
+export class HomePageComponent {
+  protected isAuthenticated$: Observable<boolean> = this.auth.isAuthenticated$;
+  protected isLoading$: Observable<boolean> = this.auth.isLoading$;
+  public constructor(private auth: AuthService, private router: Router) {}
+
+  protected login(): void {
+    this.auth.loginWithRedirect();
+  }
+
+  protected redirectToDashboard(): void {
+    this.router.navigate(['/dashboard']);
+  }
+}
