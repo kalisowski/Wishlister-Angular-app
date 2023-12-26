c0bf8ba75754be9dc724f9feb359e01b71260cb9
kalisowski <lisekgda3@gmail.com>
2023-12-26 07:30:06 +0100

    feat(api) added GamesApiService to handle API requests

---
+++ src/app/features/services/games-api.service.ts
@@ -0,0 +1,33 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+import { type Observable } from 'rxjs';
+import { type Game } from '../dto/game.model';
+import { environment } from 'src/environments/environment';
+import { CreateGame } from '../dto/create-game.model';
+import { HttpHeaders } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class GamesApiService {
+  public constructor(private httpClient: HttpClient) {}
+
+  public getGames(): Observable<Game[]> {
+    return this.httpClient.get<Game[]>(environment.API_URL + '/games');
+  }
+
+  // ...
+
+  public createGame(game: CreateGame): Observable<Game> {
+    const headers: HttpHeaders = new HttpHeaders({ 'content-type': 'application/json' });
+
+    return this.httpClient.post<Game>(`${environment.API_URL}/games`, game, { headers: headers });
+  }
+
+  // ...
+
+  public deleteGame(id: string): Observable<void> {
+    return this.httpClient.delete<void>(`${environment.API_URL}/games/${id}`);
+  }
+}
