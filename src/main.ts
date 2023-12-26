e7afc4f71400827e34bfb99bbc3c0915ea601483
kalisowski <lisekgda3@gmail.com>
2023-12-26 07:27:45 +0100

    refactor(providers)!: removed unsusedAuthClient config

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,6 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
-import { AuthClientConfig } from '@auth0/auth0-angular';
-
-new AuthClientConfig({
-  domain: 'dev-71cjpqt0pxlw04sl.us.auth0.com',
-  clientId: '6PwvJ0QOACVUe2aoJHfi2jS5DtNGZbb2',
-});
 
 bootstrapApplication(AppComponent, appConfig)
   // eslint-disable-next-line no-console
