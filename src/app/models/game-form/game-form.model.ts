cceb322de30aeafc2e09a41342d152968c16399d
kalisowski <lisekgda3@gmail.com>
2023-12-26 07:32:05 +0100

    feat(interfaces)?: added exta interfaces for data parsing due to linter (may be unnecessary)

--- src/app/models/game-form/game-form.model.ts
+++ src/app/models/game-form/game-form.model.ts
@@ -1,4 +1,5 @@
 import { FormArray, FormControl, FormGroup } from '@angular/forms';
+import { TagItem } from './tags.model';
 
 export interface GameForm {
   readonly title: FormControl<string | null>;
@@ -9,9 +10,38 @@ export interface GameForm {
   readonly wishlistPriority: FormControl<number | null>;
   readonly releaseStatus: FormControl<boolean | null>;
   readonly releaseDate: FormControl<Date | null>;
-  readonly selectedTags: FormControl<string[] | null>;
+  readonly tags: FormControl<TagItem[] | null>;
   readonly purchaseLinks: FormArray<FormGroup<PurchaseLinksForm>>;
   readonly personalNotes: FormControl<string | null>;
+}
+
+export interface ParsedForm {
+  readonly title: string | null;
+  readonly platform: string | null;
+  readonly genre: string | null;
+  readonly developer: string | null;
+  readonly price: number | null;
+  readonly wishlistPriority: number | null;
+  readonly releaseStatus: boolean | null;
+  readonly releaseDate: Date | null;
+  readonly tags: TagItem[] | null;
+  readonly purchaseLinks: { readonly purchaseLink: string | null }[];
+  readonly personalNotes: string | null;
+}
+
+export interface UpdatedForm {
+  readonly purchaseLinks: string[];
+  readonly tags: string[];
+  readonly owner: string;
+  readonly title: string;
+  readonly platform: string;
+  readonly genre: string;
+  readonly developer: string;
+  readonly price: number;
+  readonly wishlistPriority: number;
+  readonly releaseStatus: boolean;
+  readonly releaseDate: Date | null;
+  readonly personalNotes: string | null;
 }
 
 export interface PurchaseLinksForm {
