0ad4a2f485a7f5aeb839b05f2a6fd7fe73ef8223
kalisowski <lisekgda3@gmail.com>
2024-01-14 03:42:10 +0100

    feat: addded new methods for form manipulation

--- src/app/services/game-form.service.ts
+++ src/app/services/game-form.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
 import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
-import { GameForm, PurchaseLinksForm } from '../models/game-form/game-form.model';
-import { TagItem, groupedTags } from '../models/game-form/tags.model';
+import { type GameForm, type ParsedForm, type PurchaseLinksForm } from '../models/game-form/game-form.model';
+import { type TagItem, groupedTags } from '../models/game-form/tags.model';
+import { type Game } from '../features/dto/game.model';
 
 @Injectable({
   providedIn: 'root',
@@ -13,16 +14,51 @@ export class GameFormService {
   public gameForm: FormGroup<GameForm> = new FormGroup<GameForm>({
     title: new FormControl<string | null>(null, [Validators.required, Validators.maxLength(50)]),
     platform: new FormControl<string | null>(null, [Validators.required, Validators.maxLength(50)]),
-    genre: new FormControl<string | null>(null, [Validators.required, Validators.maxLength(50)]),
     developer: new FormControl<string | null>(null, [Validators.required, Validators.maxLength(50)]),
     price: new FormControl<number | null>(null, [Validators.required, Validators.min(0)]),
     wishlistPriority: new FormControl<number | null>(null, [Validators.required, Validators.min(1), Validators.max(5)]),
-    releaseStatus: new FormControl<boolean>(false),
+    releaseStatus: new FormControl<boolean | null>(false, []),
     releaseDate: new FormControl(null),
-    tags: new FormControl<TagItem[]>([]),
+    tags: new FormControl<TagItem[]>([], [Validators.required]),
     purchaseLinks: new FormArray<FormGroup<PurchaseLinksForm>>([]),
     personalNotes: new FormControl(null, Validators.maxLength(255)),
   });
+
+  public setFormValues(game: Game): void {
+    const purchaseLinks: FormArray = this.gameForm.get('purchaseLinks') as FormArray;
+    while (purchaseLinks.length !== 0) {
+      purchaseLinks.removeAt(0);
+    }
+
+    game.purchaseLinks.forEach((purchaseLink: string) => {
+      purchaseLinks.push(
+        new FormGroup({
+          purchaseLink: new FormControl(purchaseLink, [Validators.required]),
+        })
+      );
+    });
+
+    const formPartialValues: ParsedForm = {
+      title: game.title,
+      platform: game.platform,
+      developer: game.developer,
+      price: game.price,
+      wishlistPriority: game.wishlistPriority,
+      releaseStatus: game.releaseStatus,
+      tags: game.tags.map((tag: string) => ({ label: tag, value: tag })),
+      purchaseLinks: purchaseLinks.value,
+      releaseDate: null,
+      personalNotes: game.personalNotes,
+    };
+
+    let formValues: ParsedForm = { ...formPartialValues };
+
+    if (game.releaseDate) {
+      formValues = { ...formPartialValues, releaseDate: new Date(game.releaseDate) };
+    }
+
+    this.gameForm.setValue(formValues);
+  }
 
   public addPurchaseLink(): void {
     const purchaseLinks: FormArray = this.gameForm.get('purchaseLinks') as FormArray;
@@ -30,9 +66,7 @@ export class GameFormService {
       new FormGroup({
         purchaseLink: new FormControl(null, [
           Validators.required,
-          Validators.pattern(
-            `(https?://(?:www.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9].[^s]{2,}|www.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9].[^s]{2,}|https?://(?:www.|(?!www))[a-zA-Z0-9]+.[^s]{2,}|www.[a-zA-Z0-9]+.[^s]{2,})`
-          ),
+          Validators.pattern(`^(http://www.|https://www.|http://|https://)[a-zA-Z0-9]+([-.]{1}[a-zA-Z0-9]+)*.[a-zA-Z]{2,5}(:[0-9]{1,5})?(/.*)?$`),
         ]),
       })
     );
