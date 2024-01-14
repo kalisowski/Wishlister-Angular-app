b31f7e5b86f0efc57feb44216ca693c5da60e88c
kalisowski <lisekgda3@gmail.com>
2024-01-14 03:41:25 +0100

    refactor: new prittier config applied

--- src/app/validators/date-required-if.directive.ts
+++ src/app/validators/date-required-if.directive.ts
@@ -1,27 +1,16 @@
-import {
-  NG_VALIDATORS,
-  Validator,
-  AbstractControl,
-  ValidationErrors,
-  FormGroup,
-} from '@angular/forms';
+import { NG_VALIDATORS, type Validator, type AbstractControl, type ValidationErrors, type FormGroup } from '@angular/forms';
 import { Directive } from '@angular/core';
 
 @Directive({
   selector: '[appDateRequired]',
   standalone: true,
-  providers: [
-    { provide: NG_VALIDATORS, useExisting: DateRequiredDirective, multi: true },
-  ],
+  providers: [{ provide: NG_VALIDATORS, useExisting: DateRequiredDirective, multi: true }],
 })
 export class DateRequiredDirective implements Validator {
   public validate(control: AbstractControl): ValidationErrors | null {
-    const releaseStatus: boolean = (control as FormGroup).controls[
-      'releaseStatus'
-    ].value;
+    const releaseStatus: boolean = (control as FormGroup).controls['releaseStatus'].value;
 
-    const releaseDate: boolean = (control as FormGroup).controls['releaseDate']
-      .value;
+    const releaseDate: boolean = (control as FormGroup).controls['releaseDate'].value;
 
     if (releaseStatus && !releaseDate) {
       return { dateRequired: true };
