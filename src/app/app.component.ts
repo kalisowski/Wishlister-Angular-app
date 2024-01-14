bc90733be4279de72a50c827798291d1c0ffb381
kalisowski <lisekgda3@gmail.com>
2024-01-14 03:33:53 +0100

    feat: added toasts to project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-import { PrimeNGConfig } from 'primeng/api';
+import { ConfirmationService, MessageService, PrimeNGConfig } from 'primeng/api';
 import { NavComponent } from './components/nav/nav.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { DividerModule } from 'primeng/divider';
+import { ToastModule } from 'primeng/toast';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [
-    CommonModule,
-    RouterOutlet,
-    NavComponent,
-    FooterComponent,
-    DividerModule,
-  ],
+  imports: [CommonModule, RouterOutlet, NavComponent, FooterComponent, DividerModule, ToastModule],
   templateUrl: './app.component.html',
+  providers: [MessageService, ConfirmationService],
 })
 export class AppComponent implements OnInit {
   public title: string = 'Wishlister';
