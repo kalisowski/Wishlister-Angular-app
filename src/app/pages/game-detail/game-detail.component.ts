defd45b176f406b61a100b8b959e1f2e9d31c2e1
kalisowski <lisekgda3@gmail.com>
2024-01-16 11:37:16 +0100

    feat: added game detail page and logic

--- src/app/pages/game-detail/game-detail.component.ts
+++ src/app/pages/game-detail/game-detail.component.ts
@@ -8,13 +8,33 @@ import { CardModule } from 'primeng/card';
 import { ButtonModule } from 'primeng/button';
 import { ConfirmationService, MessageService } from 'primeng/api';
 import { ProgressSpinnerModule } from 'primeng/progressspinner';
-import { ConfirmDialogModule } from 'primeng/confirmdialog';
 import { BadgeModule } from 'primeng/badge';
+import { TooltipModule } from 'primeng/tooltip';
+import { ChipModule } from 'primeng/chip';
+import { NotFoundComponent } from '../not-found/not-found.component';
+import { InputGroupModule } from 'primeng/inputgroup';
+import { InputGroupAddonModule } from 'primeng/inputgroupaddon';
+import { RippleModule } from 'primeng/ripple';
+import { DlcApiService } from 'src/app/features/services/dlc-api.service';
+import { Dlc } from 'src/app/features/dto/dlc.model';
 
 @Component({
   selector: 'app-game-detail',
   standalone: true,
-  imports: [CommonModule, CardModule, ButtonModule, RouterLink, ProgressSpinnerModule, ConfirmDialogModule, BadgeModule],
+  imports: [
+    RippleModule,
+    InputGroupModule,
+    InputGroupAddonModule,
+    CommonModule,
+    CardModule,
+    ButtonModule,
+    RouterLink,
+    ProgressSpinnerModule,
+    BadgeModule,
+    TooltipModule,
+    ChipModule,
+    NotFoundComponent,
+  ],
   templateUrl: './game-detail.component.html',
 })
 export class GameDetailComponent implements OnInit {
@@ -24,20 +44,53 @@ export class GameDetailComponent implements OnInit {
   public constructor(
     private route: ActivatedRoute,
     private gamesApiService: GamesApiService,
+    private dlcApiService: DlcApiService,
     private router: Router,
     private confirmationService: ConfirmationService,
     private messageService: MessageService
   ) {}
 
-  public confirm(): void {
+  public ngOnInit(): void {
+    const id: string | null = this.route.snapshot.paramMap.get('id');
+    if (id) {
+      this.gamesApiService.getGame(id).subscribe({
+        next: (game: Game) => {
+          this.game = game;
+          this.loading = false;
+        },
+        error: (error: HttpErrorResponse) => {
+          this.error = error;
+          if (this.error.status === 404) {
+            this.router.navigate(['/error']);
+          }
+          this.loading = false;
+        },
+      });
+    }
+  }
+
+  public confirmGameDeletion(): void {
     this.confirmationService.confirm({
-      header: 'Are you sure?',
-      message: 'Please confirm to proceed.',
+      header: 'You are about to delete a game',
+      message: `This will also delete all DLCs associated with this game!`,
       accept: () => {
         this.deleteGame();
       },
       reject: () => {
-        this.messageService.add({ severity: 'info', summary: 'Canceled', detail: 'You have canceled the deletion' });
+        this.messageService.add({ severity: 'info', summary: 'Canceled', detail: 'You have canceled the game deletion' });
+      },
+    });
+  }
+
+  public confirmDlcDeletion(dlcId: string): void {
+    this.confirmationService.confirm({
+      header: 'You are about to delete a DLC',
+      message: 'Please confirm to proceed.',
+      accept: () => {
+        this.deleteDlc(dlcId);
+      },
+      reject: () => {
+        this.messageService.add({ severity: 'info', summary: 'Canceled', detail: 'You have canceled the dlc deletion' });
       },
     });
   }
@@ -55,24 +108,27 @@ export class GameDetailComponent implements OnInit {
         error: (error: HttpErrorResponse) => {
           this.error = error;
           this.messageService.add({ severity: 'error', summary: 'Error', detail: `Failed deleting element - ${this.error.status}` });
-          console.error(error);
         },
       });
     }
   }
 
-  public ngOnInit(): void {
+  public deleteDlc(dlcId: string): void {
     const id: string | null = this.route.snapshot.paramMap.get('id');
+
     if (id) {
-      this.gamesApiService.getGame(id).subscribe({
-        next: (game: Game) => {
-          this.game = game;
-          this.loading = false;
+      this.dlcApiService.deleteDlc(id, dlcId).subscribe({
+        next: () => {
+          this.messageService.add({ severity: 'success', summary: 'Success', detail: 'Successfully removed dlc' });
+          if (this.game) {
+            this.game = {
+              ...this.game,
+              dlcs: this.game.dlcs.filter((dlc: Dlc) => dlc.dlcId !== dlcId),
+            };
+          }
         },
         error: (error: HttpErrorResponse) => {
-          this.error = error;
-          this.loading = false;
-          console.error(error);
+          this.messageService.add({ severity: 'error', summary: 'Error', detail: `Failed deleting element - ${error.status}` });
         },
       });
     }
