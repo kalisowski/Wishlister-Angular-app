d52328074bdf19387077744ace92d5df6b8de615
kalisowski <lisekgda3@gmail.com>
2024-01-16 11:37:59 +0100

    feat: added gameform action confirmations (WIP)

--- src/app/pages/game-form/game-form.component.ts
+++ src/app/pages/game-form/game-form.component.ts
@@ -19,6 +19,11 @@ import { ValidationDisplayService } from 'src/app/services/validation-display.se
 import { GamesApiService } from 'src/app/features/services/games-api.service';
 import { type Game } from 'src/app/features/dto/game.model';
 import { AuthUserService } from 'src/app/services/auth-user.service';
+import { ProgressSpinnerModule } from 'primeng/progressspinner';
+import { ConfirmationService, MessageService } from 'primeng/api';
+import { ThisReceiver } from '@angular/compiler';
+import { EMPTY, catchError, throwError } from 'rxjs';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-game-form',
@@ -36,18 +41,19 @@ import { AuthUserService } from 'src/app/services/auth-user.service';
     InputTextModule,
     CalendarModule,
     ToggleButtonModule,
+    ProgressSpinnerModule,
   ],
-  animations: [],
   templateUrl: './game-form.component.html',
 })
 export class GameFormComponent implements OnInit {
   public editForm: boolean = false;
-  protected gameForm: FormGroup<GameForm> = this.gameFormService.gameForm;
-  protected tags: TagItem[] = this.gameFormService.tags;
-  protected date: Date = new Date();
-  protected tomorrow: string = '';
-  protected gameId: string | null = null;
+  public gameForm: FormGroup<GameForm> = this.gameFormService.gameForm;
+  public tags: TagItem[] = this.gameFormService.tags;
+  public date: Date = new Date();
+  public tomorrow: string = '';
+  public gameId: string | null = null;
   private initialFormState!: Game;
+  public loading: boolean = false;
 
   public constructor(
     private router: Router,
@@ -56,18 +62,23 @@ export class GameFormComponent implements OnInit {
     private validationDisplay: ValidationDisplayService,
     private gamesApiService: GamesApiService,
     private authUser: AuthUserService,
+    private confirmationService: ConfirmationService,
+    private messageService: MessageService,
     private location: Location
   ) {}
 
   public ngOnInit(): void {
+    this.resetForm();
     const date: Date = new Date();
     this.date.setDate(date.getDate() + 1);
     this.gameId = this.route.snapshot.paramMap.get('id');
     if (this.gameId) {
+      this.loading = true;
       this.editForm = true;
       this.gamesApiService.getGame(this.gameId).subscribe((game: Game) => {
         this.gameFormService.setFormValues(game);
         this.initialFormState = game;
+        this.loading = false;
       });
     }
   }
@@ -106,6 +117,7 @@ export class GameFormComponent implements OnInit {
   }
 
   public resetForm(): void {
+    this.gameForm.reset();
     this.gameFormService.resetFormDynamicFields();
 
     if (this.editForm) {
@@ -113,40 +125,84 @@ export class GameFormComponent implements OnInit {
     }
   }
 
-  private createUpdatedForm(parsedForm: ParsedForm, userId: string): UpdatedForm {
-    return {
-      ...parsedForm,
-      purchaseLinks: parsedForm.purchaseLinks!.map((link: { purchaseLink: string | null }) => (link.purchaseLink ? link.purchaseLink : '')),
-      tags: parsedForm.tags?.map((tag: TagItem) => tag.value) ?? [],
-      owner: userId,
-      title: parsedForm.title || '',
-      platform: parsedForm.platform || '',
-      developer: parsedForm.developer || '',
-      price: parsedForm.price || 0,
-      wishlistPriority: parsedForm.wishlistPriority || 0,
-      releaseStatus: parsedForm.releaseStatus || false,
-    };
+  public confirmFormSubmit(): void {
+    this.confirmationService.confirm({
+      header: 'You are about to submit the form',
+      message: `This add or update the game in the database!`,
+      accept: () => {
+        this.sendForm();
+      },
+    });
+  }
+
+  public confirmFormReset(): void {
+    this.confirmationService.confirm({
+      header: 'You are about to reset the form',
+      message: `This will revert the form to its initial state!`,
+      accept: () => {
+        this.resetForm();
+        this.messageService.add({ severity: 'info', summary: 'Reset', detail: 'Form has been reset' });
+      },
+    });
+  }
+
+  public confirmFormCancel(): void {
+    this.confirmationService.confirm({
+      header: 'You are about to leave the form',
+      message: `This will discard all changes!`,
+      accept: () => {
+        this.goBack();
+        this.messageService.add({ severity: 'info', summary: 'Canceled', detail: 'Form has been discarded' });
+      },
+    });
   }
 
   protected onSubmit(): void {
     if (this.gameForm.invalid) {
       this.gameForm.markAllAsTouched();
+      console.log(this.gameForm);
 
       return;
     }
+    this.confirmFormSubmit();
+  }
 
+  private sendForm(): void {
     const parsedForm: ParsedForm = this.gameForm.getRawValue();
+
     this.authUser.getUserId().subscribe((userId: string) => {
-      const updatedForm: UpdatedForm = this.createUpdatedForm(parsedForm, userId);
+      const updatedForm: UpdatedForm = this.gameFormService.createUpdatedForm(parsedForm, userId);
 
       if (this.gameId) {
-        this.gamesApiService.updateGame(this.gameId, updatedForm).subscribe(() => {
-          this.router.navigate(['/game', this.gameId]);
-        });
+        this.gamesApiService
+          .updateGame(this.gameId, updatedForm)
+          .pipe(
+            catchError((error: HttpErrorResponse) => {
+              console.log(error);
+              this.messageService.add({ severity: 'error', summary: `Error ${error.status}`, detail: `Failed to update the game: ${error.statusText}` });
+
+              return EMPTY;
+            })
+          )
+          .subscribe(() => {
+            this.messageService.add({ severity: 'success', summary: 'Success', detail: 'Game has been updated' });
+            this.router.navigate(['/game', this.gameId]);
+          });
       } else {
-        this.gamesApiService.createGame(updatedForm).subscribe(() => {
-          this.router.navigate(['/dashboard']);
-        });
+        this.gamesApiService
+          .createGame(updatedForm)
+          .pipe(
+            catchError((error: HttpErrorResponse) => {
+              console.log(error);
+              this.messageService.add({ severity: 'error', summary: `Error ${error.status}`, detail: `Failed to create the game: ${error.statusText}` });
+
+              return EMPTY;
+            })
+          )
+          .subscribe(() => {
+            this.messageService.add({ severity: 'success', summary: 'Success', detail: 'Game has been added' });
+            this.router.navigate(['/dashboard']);
+          });
       }
     });
   }
