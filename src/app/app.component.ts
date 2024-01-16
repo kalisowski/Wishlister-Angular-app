11eca6676136d472ee15c0c1cea17cc06c23d9b0
kalisowski <lisekgda3@gmail.com>
2024-01-16 11:31:59 +0100

    feat: added global confirmation dialog

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import type { OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-import { ConfirmationService, MessageService, PrimeNGConfig } from 'primeng/api';
+import { PrimeNGConfig } from 'primeng/api';
+import { ConfirmationService, MessageService } from 'primeng/api';
 import { NavComponent } from './components/nav/nav.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { DividerModule } from 'primeng/divider';
 import { ToastModule } from 'primeng/toast';
+import { ConfirmDialogModule } from 'primeng/confirmdialog';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, NavComponent, FooterComponent, DividerModule, ToastModule],
+  imports: [CommonModule, RouterOutlet, NavComponent, FooterComponent, DividerModule, ToastModule, ConfirmDialogModule],
   templateUrl: './app.component.html',
   providers: [MessageService, ConfirmationService],
 })
